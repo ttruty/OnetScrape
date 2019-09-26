d03847568961ae52d31614c471a09f83d81546bb
Timothy Truty <ttruty@gmail.com>
2019-09-25 19:47:03 -0500

    initial commit

---
+++ GoogleSheet.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+from OnetWebService import OnetWebService
+import sys
+
+username = ''
+password = ''
+onet_ws = OnetWebService(username, password)
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1l2aL3pliJWK8I3Nq3fri3zQdjQZQtGaHY5lSeFLo5aQ'
+SAMPLE_RANGE_NAME = 'Rapid Growth!A5:A393'
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        #print('Name, ID:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            #print(row[0])
+            request = 'mnm/careers/' + row[0] + '/job_outlook'
+            kwresults = onet_ws.call(request)
+            # check_for_error(kwresults)
+            # if (not 'occupation' in kwresults) or (0 == len(kwresults['occupation'])):
+            #     print("No relevant occupations were found.")
+            #     print("")
+            # else:
+            #     print("Most relevant occupations for \"" + kwquery + "\":")
+            #     for occ in kwresults['occupation']:
+            #         print("  " + occ['code'] + " - " + occ['title'])
+            #     print("")
+            try:
+                print(kwresults['salary']['annual_90th_percentile'])
+            except:
+                print("No salary info")
+
+if __name__ == '__main__':
+    main()
+
+
+
